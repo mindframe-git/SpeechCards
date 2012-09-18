32b65bb7a8f7fbaf5cfafd93e89c66a5aa225eb5
MinDFrame <mindframe@MindTeam.(none)>
2012-09-18 17:03:40 +0200

    -Añadidos controles en el menú. -Borrados botones.

--- src/com/mindframe/speechcards/EditSpeechActivity.java
+++ src/com/mindframe/speechcards/EditSpeechActivity.java
@@ -26,281 +26,211 @@ import android.widget.Toast;
 /**
  * 
  * @author mindframe
- *
- *Esta clase cargará en la plantilla de edición las tarjetas pertenecientes a un discurso,
- *puede navegar entre ellas, añadir, modificar y borrar tarjetas.
- *
+ * 
+ *         Esta clase cargará en la plantilla de edición las tarjetas
+ *         pertenecientes a un discurso, puede navegar entre ellas, añadir,
+ *         modificar y borrar tarjetas.
+ * 
  */
 public class EditSpeechActivity extends Activity {
-	
+
 	List<Card> cardList;
 	Card currentCard;
-	Context context; 
+	Context context;
 	BaseDatosHelper bdh;
 	TextView tvTitleSpeech;
 	EditText etHeader, etBody;
-	Button btnPrevCard, btnNextCard, btnAdd, btnDeleteCard, btnMod;
+	Button btnPrevCard, btnNextCard, btnMod;
 	int id_speech;
 	boolean addingCard;
-	
-	
+
 	public void onCreate(Bundle savedInstanceState) {
-		
+
 		getWindowManager().getDefaultDisplay().getHeight();
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_speech);
-		
-		
-		tvTitleSpeech = (TextView)findViewById(R.id.tvTitleSpeech);
-		etHeader = (EditText)findViewById(R.id.etHeader);
-		etBody = (EditText)findViewById(R.id.etBody);
-		btnNextCard = (Button)findViewById(R.id.btnNextCard);
-		btnPrevCard = (Button)findViewById(R.id.btnPrevCard);
-		btnMod = (Button)findViewById(R.id.btnMod);
-		btnDeleteCard = (Button)findViewById(R.id.btnDeleteCard);
-		btnAdd = (Button)findViewById(R.id.btnAdd);
-		
+
+		tvTitleSpeech = (TextView) findViewById(R.id.tvTitleSpeech);
+		etHeader = (EditText) findViewById(R.id.etHeader);
+		etBody = (EditText) findViewById(R.id.etBody);
+		btnNextCard = (Button) findViewById(R.id.btnNextCard);
+		btnPrevCard = (Button) findViewById(R.id.btnPrevCard);
+		btnMod = (Button) findViewById(R.id.btnMod);
+
 		cardList = new ArrayList<Card>();
 		context = this.getApplicationContext();
-		
+
 		bdh = new BaseDatosHelper(context, "SpeechCards", null, 1);
 		addingCard = false;
-		
-		
+
 		Bundle bundle = getIntent().getExtras();
-		
-		id_speech =  bundle.getInt("id_speech");
+
+		id_speech = bundle.getInt("id_speech");
 		tvTitleSpeech.setText(bundle.getString("speechTitle"));
-		
+
 		cardList = bdh.getCardsByIdSpeech(id_speech);
-		
-		if(cardList.isEmpty()){
+
+		if (cardList.isEmpty()) {
 			currentCard = null;
 			etHeader.setText("");
 			etBody.setText("");
 		}
-		
+
 		currentCard = getFirstCard();
-		
+
 		writeCard(currentCard);
-		
-		
+
 		btnPrevCard.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				getPrevCard();
-				
+
 			}
 		});
-		
-		
+
 		btnNextCard.setOnClickListener(new OnClickListener() {
-			
+
 			@SuppressLint("ParserError")
 			@Override
 			public void onClick(View v) {
 				getNextCard();
 			}
 		});
-		
+
 		btnMod.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				
-				if(addingCard){
+
+				if (addingCard) {
 					Card card = new Card();
 					card.body = etBody.getText().toString();
 					card.header = etHeader.getText().toString();
 					card.id_prev_card = currentCard.id_card;
 					card.id_next_card = currentCard.id_next_card;
 					card.id_speech = id_speech;
-					if(etBody.getText().toString().trim().compareToIgnoreCase("") != 0){
+					if (etBody.getText().toString().trim().compareToIgnoreCase("") != 0) {
 						int idNewCard = bdh.addCard(card, true);
 						addingCard = false;
 						Toast.makeText(context, "Se ha añadido la tarjeta.", Toast.LENGTH_SHORT).show();
-						
+
 						cardList = bdh.getCardsByIdSpeech(id_speech);
-						
+
 						currentCard = getCardById(idNewCard);
-					}else{
+					} else {
 						Toast.makeText(context, "Debe escribir algo en el cuerpo.", Toast.LENGTH_SHORT).show();
 					}
-					
-				}else{
-					if(currentCard != null && !addingCard){
-						if(etBody.getText().toString().trim().compareToIgnoreCase("") != 0){
-							if(bdh.modCard(currentCard, etHeader.getText().toString(), etBody.getText().toString())){
+
+				} else {
+					if (currentCard != null && !addingCard) {
+						if (etBody.getText().toString().trim().compareToIgnoreCase("") != 0) {
+							if (bdh.modCard(currentCard, etHeader.getText().toString(), etBody.getText().toString())) {
 								Toast.makeText(context, "Se ha actualizado la tarjeta.", Toast.LENGTH_SHORT).show();
 								cardList = bdh.getCardsByIdSpeech(id_speech);
-							}else{
+							} else {
 								Toast.makeText(context, "Algo ha fallado :S", Toast.LENGTH_SHORT).show();
 							}
-						}else{
+						} else {
 							Toast.makeText(context, "Debe escribir algo en el cuerpo.", Toast.LENGTH_SHORT).show();
 						}
-					}else{
-						Toast.makeText(context, "No hay tarjetas", Toast.LENGTH_SHORT).show();
-					}
-				}
-				
-			}
-		});
-		
-		btnDeleteCard.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				if(currentCard != null){
-					int result = bdh.delCard(currentCard);
-					
-					if(result != 1){
-						Toast.makeText(context, "Algo ha fallado :S", Toast.LENGTH_SHORT).show();
-					}else{
-						Toast.makeText(context, "Tarjeta borrada.", Toast.LENGTH_SHORT).show();
-						
-						if(currentCard.isFirst() && currentCard.isLast()){
-							//Es la última tarjeta
-							Toast.makeText(context, "No hay más tarjetas.", Toast.LENGTH_SHORT).show();
-							etBody.setText("");
-							etHeader.setText("");
-							currentCard = null;
-						}else{
-							
+					} else {
+						// No hay tarjetas y vamos a añadir la primera.
+
+						if (etBody.getText().toString().trim().compareToIgnoreCase("") != 0) {
+							Card newCard = new Card();
+							newCard.id_speech = id_speech;
+							newCard.id_next_card = 0;
+							newCard.id_prev_card = 0;
+							newCard.body = etBody.getText().toString();
+							newCard.header = etHeader.getText().toString();
+
+							bdh.addCard(newCard, false);
+
 							cardList = bdh.getCardsByIdSpeech(id_speech);
-							
-							if(currentCard.isLast())
-								getPrevCard();
-							if(currentCard.isFirst())
-								getNextCard();
-							if(!currentCard.isFirst() && !currentCard.isLast())
-								getPrevCard();
+							currentCard = getFirstCard();
+
+							Toast.makeText(context, "Tarjeta agregada.", Toast.LENGTH_SHORT).show();
+
+						} else {
+							Toast.makeText(context, "Debe escribir algo en el cuerpo.", Toast.LENGTH_SHORT).show();
 						}
-						
-						
 					}
-				}else
-					Toast.makeText(context, "No hay más tarjetas.", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		btnAdd.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				/*
-				 * Varios escenarios:
-				 * 1) No hay tarjeta.
-				 * 2) Es una tarjeta de enmedio
-				 */
-				
-				//Escenario: No hay ninguna tarjeta:
-				//la pantalla estará en blanco, se agregara con prev y next a 0
-				//se establecerá esta nueva a currentCard y se recargará la lista.
-				if(currentCard == null){
-					if(etBody.getText().toString().trim().compareToIgnoreCase("") != 0){
-						Card newCard = new Card();
-						newCard.id_speech = id_speech;
-						newCard.id_next_card = 0;
-						newCard.id_prev_card = 0;
-						newCard.body = etBody.getText().toString();
-						newCard.header = etHeader.getText().toString();
-						
-						bdh.addCard(newCard, false);
-						
-						cardList = bdh.getCardsByIdSpeech(id_speech);
-						currentCard = getFirstCard();
-						
-						Toast.makeText(context, "Tarjeta agregada.", Toast.LENGTH_SHORT).show();
-						
-					}else{
-						Toast.makeText(context, "Debe escribir algo en el cuerpo.", Toast.LENGTH_SHORT).show();
-					}
-				}else{
-					/*
-					 * Escenario: Es una tarjeta de enmedio
-					 * Se ponen los campos en blanco.
-					 * newCard.prev = curr.id
-					 * newCard.next = curr.next
-					 */
-					etBody.setText("");
-					etHeader.setText("");
-					addingCard = true;
-					
-					Toast.makeText(context, "Pulse modificar para terminar.", Toast.LENGTH_SHORT).show();
 				}
+
 			}
 		});
 	
+
 	}
-	
+
 	private Card getFirstCard() {
-		for(Card card : cardList){
-			if(card.id_prev_card == 0)
+		for (Card card : cardList) {
+			if (card.id_prev_card == 0)
 				return card;
 		}
 		return null;
 	}
-	
+
 	private void writeCard(Card card) {
-		if(card != null){
-			if(card.getHeader() != null)
+		if (card != null) {
+			if (card.getHeader() != null)
 				etHeader.setText(card.getHeader());
-			if(card.getBody() != null)
+			if (card.getBody() != null)
 				etBody.setText(card.getBody());
 		}
 	}
-	
-	private void getNextCard(){
-		if(currentCard != null){
-			if(currentCard.isLast()){
+
+	private void getNextCard() {
+		if (currentCard != null) {
+			if (currentCard.isLast()) {
 				Toast.makeText(context, "No hay más tarjetas.", Toast.LENGTH_SHORT).show();
-			}else{
-				for(Card card : cardList){
-					if(card.getId_card() == currentCard.getId_next_card()){
+			} else {
+				for (Card card : cardList) {
+					if (card.getId_card() == currentCard.getId_next_card()) {
 						currentCard = card;
 						writeCard(card);
 						break;
 					}
 				}
 			}
-		}else{
+		} else {
 			Toast.makeText(context, "No hay tarjetas.", Toast.LENGTH_SHORT).show();
 		}
-		
+
 	}
-	
-	private void getPrevCard(){
-		if(currentCard != null){
-			if(currentCard.isFirst()){
+
+	private void getPrevCard() {
+		if (currentCard != null) {
+			if (currentCard.isFirst()) {
 				Toast.makeText(context, "Se encuentra en la primera tarjeta.", Toast.LENGTH_SHORT).show();
-			}else{
-				for(Card card : cardList){
-					if(card.id_card == currentCard.getId_prev_card()){
+			} else {
+				for (Card card : cardList) {
+					if (card.id_card == currentCard.getId_prev_card()) {
 						currentCard = card;
 						writeCard(card);
 						break;
 					}
 				}
 			}
-		}else{
+		} else {
 			Toast.makeText(context, "No hay tarjetas.", Toast.LENGTH_SHORT).show();
 		}
 	}
-	public Card getCardById(int idCard){
-		
-		for(Card card : cardList){
-			if(card.id_card == idCard){
+
+	public Card getCardById(int idCard) {
+
+		for (Card card : cardList) {
+			if (card.id_card == idCard) {
 				return card;
 			}
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -309,8 +239,7 @@ public class EditSpeechActivity extends Activity {
 		inflater.inflate(R.menu.menu_edit, menu);
 		return true;
 	}
-	
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -318,20 +247,50 @@ public class EditSpeechActivity extends Activity {
 		case R.id.mnDelSpeech:
 			crearDialogoConfirmacion();
 			return true;
-		
+
 		case R.id.mnAddCard:
-			
+			etBody.setText("");
+			etHeader.setText("");
+			addingCard = true;
 			return true;
 
 		case R.id.mnDelCard:
-			
+			if (currentCard != null) {
+				int result = bdh.delCard(currentCard);
+
+				if (result != 1) {
+					Toast.makeText(context, "Algo ha fallado :S", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(context, "Tarjeta borrada.", Toast.LENGTH_SHORT).show();
+
+					if (currentCard.isFirst() && currentCard.isLast()) {
+						// Es la última tarjeta
+						Toast.makeText(context, "No hay más tarjetas.", Toast.LENGTH_SHORT).show();
+						etBody.setText("");
+						etHeader.setText("");
+						currentCard = null;
+					} else {
+
+						cardList = bdh.getCardsByIdSpeech(id_speech);
+
+						if (currentCard.isLast())
+							getPrevCard();
+						if (currentCard.isFirst())
+							getNextCard();
+						if (!currentCard.isFirst() && !currentCard.isLast())
+							getPrevCard();
+					}
+				}
+			} else {
+				Toast.makeText(context, "No hay tarjetas.", Toast.LENGTH_SHORT).show();
+			}
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 
 		}
 	}
-	
+
 	private Dialog crearDialogoConfirmacion() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Confirmacion");
@@ -352,8 +311,4 @@ public class EditSpeechActivity extends Activity {
 		return builder.show();
 	}
 
-	
-	
-	
-	
 }
